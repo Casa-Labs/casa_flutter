a3b46652a9c1354b4b6bc5d4bf6098e4c88d7adb
Daksh <72447688+mrcodefrost@users.noreply.github.com>
2025-02-12 21:21:47 +0530

    Graphql common service layer created

---
+++ lib/network/graph_ql_mutations.dart
@@ -0,0 +1,196 @@
+class GraphQLMutations {
+  static const String registerMutation = """
+  mutation Register(\$email: String, \$password: String) {
+    register(email: \$email, password: \$password) {
+      token
+      user {
+        authProvider
+        createdAt
+        email
+        id
+        role
+        updatedAt
+      }
+    }
+  }
+""";
+
+  static const String loginMutation = """
+  mutation Login(\$email: String!, \$password: String!) {
+  login(email: \$email, password: \$password) {
+    token
+    user {
+      authProvider
+      createdAt
+      email
+      id
+      role
+      updatedAt
+    }
+  }
+}
+""";
+
+  static const String singleSignOnMutation = """
+  mutation SingleSignOn(\$email: String!, \$provider: AuthProvider!, \$providerId: String!) {
+  SingleSignOn(email: \$email, provider: \$provider, providerId: \$providerId) {
+    token
+    user {
+      authProvider
+      createdAt
+      email
+      id
+      role
+      updatedAt
+    }
+  }
+}
+""";
+
+  static const String addItemToCart = '''
+mutation AddItemToCart(\$userId: String, \$items: JSON) {
+  addItemToCart(userId: \$userId, items: \$items) {
+    createdAt
+    id
+    items
+    updatedAt
+    userId
+  }
+}
+''';
+
+  static const String createOrder = '''
+mutation CreateOrder(\$productId: String!, \$total: Float!, \$userId: String, \$orderedProductDetails: JSON, \$shippingInfo: JSON) {
+  createOrder(productId: \$productId, total: \$total, userId: \$userId, orderedProductDetails: \$orderedProductDetails, shippingInfo: \$shippingInfo) {
+    createdAt
+    id
+    orderedProductDetails
+    productId
+    shippingInfo
+    total
+    updatedAt
+    userId
+  }
+}
+''';
+
+  static const String createProduct = '''
+mutation CreateProduct(\$input: ProductInput!) {
+  createProduct(input: \$input) {
+    category
+    createdAt
+    description
+    id
+    isNewArrival
+    isTrending
+    name
+    price
+    size
+    stock
+    storeId
+    updatedAt
+  }
+}
+''';
+
+  static const String createStore = '''
+mutation CreateStore(\$userId: String, \$name: String!, \$description: String, \$type: StoreType!, \$logo: String) {
+  createStore(userId: \$userId, name: \$name, description: \$description, type: \$type, logo: \$logo) {
+    createdAt
+    description
+    id
+    isDeleted
+    logo
+    name
+    reasonToDelete
+    type
+    updatedAt
+  }
+}
+''';
+
+  static const String registerOrderReturn = '''
+mutation RegisterOrderReturn(\$userId: String!, \$orderId: String!, \$reason: String!, \$replacementId: String) {
+  registerOrderReturn(userId: \$userId, orderId: \$orderId, reason: \$reason, replacementId: \$replacementId) {
+    createdAt
+    id
+    orderId
+    reason
+    replacementId
+    status
+    updatedAt
+    userId
+  }
+}
+''';
+
+  static const String registerProductReview = '''
+mutation RegisterProductReview(\$productId: String!, \$userId: String, \$liked: Boolean, \$disliked: Boolean, \$viewed: Boolean, \$rating: Int, \$comment: String) {
+  registerProductReview(productId: \$productId, userId: \$userId, liked: \$liked, disliked: \$disliked, viewed: \$viewed, rating: \$rating, comment: \$comment) {
+    comment
+    createdAt
+    disliked
+    liked
+    productId
+    rating
+    updatedAt
+    userId
+    viewed
+  }
+}
+''';
+
+  static const String removeItemFromCart = '''
+mutation RemoveItemFromCart(\$userId: String!, \$productId: String!) {
+  removeItemFromCart(userId: \$userId, productId: \$productId) {
+    createdAt
+    id
+    items
+    updatedAt
+    userId
+  }
+}
+''';
+
+  static const String updateCartItem = '''
+mutation UpdateCartItem(\$userId: String, \$productId: String, \$quantity: Int) {
+  updateCartItem(userId: \$userId, productId: \$productId, quantity: \$quantity) {
+    createdAt
+    id
+    items
+    updatedAt
+    userId
+  }
+}
+''';
+
+  static const String updateOrder = '''
+mutation UpdateOrder(\$userId: String, \$orderId: String, \$orderedProductDetails: JSON, \$total: Float, \$shippingInfo: JSON) {
+  updateOrder(userId: \$userId, orderId: \$orderId, orderedProductDetails: \$orderedProductDetails, total: \$total, shippingInfo: \$shippingInfo) {
+    createdAt
+    id
+    orderedProductDetails
+    productId
+    shippingInfo
+    total
+    updatedAt
+    userId
+  }
+}
+''';
+
+  static const String updateOrderReturnStatus = '''
+mutation UpdateOrderReturnStatus(\$returnId: String!, \$status: String!, \$replacementId: String) {
+  updateOrderReturnStatus(returnId: \$returnId, status: \$status, replacementId: \$replacementId) {
+    createdAt
+    id
+    orderId
+    reason
+    replacementId
+    status
+    updatedAt
+    userId
+  }
+}
+''';
+}
---
+++ lib/network/graph_ql_queries.dart
@@ -0,0 +1,139 @@
+class GraphQLQueries {
+  static const String getNewArrivalProducts = """
+query GetNewArrivalProducts(\$page: Int!, \$limit: Int!, \$search: String) {
+  getNewArrivalProducts(page: \$page, limit: \$limit, search: \$search) {
+    products {
+    price
+      category
+      createdAt
+      description
+      id
+      isNewArrival
+      isTrending
+      name
+      size
+      stock
+      storeId
+      updatedAt
+    }
+  }
+}
+""";
+
+  static const String getOrderReturns = """
+query GetOrderReturns(\$userId: String!, \$orderId: String!) {
+  getOrderReturns(userId: \$userId, orderId: \$orderId) {
+    createdAt
+    id
+    orderId
+    reason
+    replacementId
+    status
+    updatedAt
+    userId
+  }
+}
+""";
+
+  static const String getOrders = """
+query GetOrders(\$userId: String!) {
+  getOrders(userId: \$userId) {
+    createdAt
+    id
+    orderedProductDetails
+    productId
+    shippingInfo
+    total
+    updatedAt
+    userId
+  }
+}
+""";
+
+  static const String getProductReviews = """
+query GetProductReviews(\$productId: String!) {
+  getProductReviews(productId: \$productId) {
+    comment
+    createdAt
+    disliked
+    liked
+    productId
+    rating
+    userId
+    updatedAt
+    viewed
+  }
+}
+""";
+
+  static const String getProducts = """
+query GetProducts(\$params: GetProduct!) {
+  getProducts(params: \$params) {
+    currentPage
+    data {
+      authProvider
+      createdAt
+      email
+      id
+      role
+      updatedAt
+    }
+    total
+    totalPages
+  }
+}
+""";
+
+  static const String getTrendingProducts = """
+query GetTrendingProducts(\$page: Int!, \$limit: Int!, \$search: String) {
+  getTrendingProducts(page: \$page, limit: \$limit, search: \$search) {
+    products {
+      category
+      createdAt
+      description
+      id
+      isNewArrival
+      isTrending
+      name
+      price
+      size
+      stock
+      storeId
+      updatedAt
+    }
+    totalCount
+  }
+}
+""";
+
+  static const String getUser = """
+query GetUser(\$getUserId: String!) {
+  getUser(id: \$getUserId) {
+    authProvider
+    createdAt
+    email
+    id
+    role
+    updatedAt
+  }
+}
+""";
+
+  static const String getUsers = """
+query GetUsers(\$page: Int!, \$limit: Int!, \$search: String) {
+  getUsers(page: \$page, limit: \$limit, search: \$search) {
+    currentPage
+    data {
+      authProvider
+      createdAt
+      email
+      id
+      role
+      updatedAt
+    }
+    total
+    totalPages
+  }
+}
+""";
+}
