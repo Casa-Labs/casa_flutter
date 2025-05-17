40f5eb2dc88798b8d8497ef04cf37f0e95524b06
“aj”1996 <dev.ashvin96@gmail.com>
2025-05-17 12:10:37 +0530

    9) in add to cart - size guide sheet does not expand on pinch. 14) homepage - not searching any products. issue resolved.

--- lib/src/cart/view/widgets/size_guide_dialog.dart
+++ lib/src/cart/view/widgets/size_guide_dialog.dart
@@ -9,11 +9,27 @@ class SizeGuideDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-        backgroundColor: BackgroundColor.white.withValues(alpha: 70),
-        insetPadding: EdgeInsets.symmetric(horizontal: 10),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Image.asset('assets/images/size_guide.jpeg')));
+      backgroundColor: Colors.transparent,
+      insetPadding: EdgeInsets.zero,
+      child: Stack(
+        children: [
+          InteractiveViewer(
+            panEnabled: true,
+            boundaryMargin: const EdgeInsets.all(100),
+            minScale: 0.8,
+            maxScale: 4.0,
+            child: Image.asset('assets/images/size_guide.jpeg'),
+          ),
+          Positioned(
+            top: 20,
+            right: 20,
+            child: IconButton(
+              icon: const Icon(Icons.close, color: Colors.white, size: 30),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
