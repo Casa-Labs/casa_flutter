7c662b70baa192bc4d151c08172c4ebaf5ac79e6
“aj”1996 <dev.ashvin96@gmail.com>
2025-05-22 21:17:33 +0530

    Uuid added as deviceID in signup api

--- lib/src/auth/controller/sign_up_controller.dart
+++ lib/src/auth/controller/sign_up_controller.dart
@@ -5,6 +5,7 @@ import 'package:casaflutter/utils/preference_manager.dart';
 import 'package:casaflutter/utils/validators.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:uuid/uuid.dart';
 
 class SignUpController extends GetxController {
   // ========= OBJECTS ============= //
@@ -133,11 +134,14 @@ class SignUpController extends GetxController {
   }
 
   Future<void> registerUserCall() async {
+    // TODO : Uuid will remove after implement live deviceID
+    var deviceId = const Uuid().v4();
+
     isRegistrationInProgress(true);
     LoginRequestModel loginRequestModel = LoginRequestModel(
       username: registeredEmail.text,
       password: registeredPassword.text,
-      deviceID: '1234', /// devise id should be change
+      deviceID: deviceId,
     );
 
     final registerUserResponse = await _authService.registerUser(
